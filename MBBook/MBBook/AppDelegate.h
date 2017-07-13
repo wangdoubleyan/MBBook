067dff7e7103a2685e808a84812014cd0727279a
SpongeBob-GitHub <1605319671@qq.com>
2017-07-13 12:11:35 +0800

    .

---
+++ MBBook/MBBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBBook
+//
+//  Created by Bing Ma on 7/13/17.
+//  Copyright © 2017 Bing Ma (Hannb). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBBook/MBBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MBBook
+//
+//  Created by Bing Ma on 7/13/17.
+//  Copyright © 2017 Bing Ma (Hannb). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
